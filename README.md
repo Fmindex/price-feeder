c76a94ad224e854a671d8018df811f8abd7b9ab4
ekkalak.leelasorn <ekkalak.leelasorn@bytedance.com>
2022-02-04 14:23:39 +0800

    add readme

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # price-feeder
+
 ### Overview
-TODO
+
+A scheduled function which keep the price in oracle contract updated.
+
+#### Key concepts
+
+- The function will periodically fetch current price from oracle contract and determine whether we need to update the price in oracle or not.
+
+- The logic is separated to be modules (`priceClient`, `terraClient`, `oracleService`, `priceFeeder`). all the modules will be injected to `priceFeeder` as its dependencies. When we want to test the service, we can inject the mocked service instead.
 
 ### Quick Start
+
 1. Install: `yarn`
 2. Run: `yarn start`
+3. Test: `yarn test`
 
-### how to add new currency
-TODO
+### how to add new symbol
+
+in the `src/feeder.ts`, add new symbol to `symbols`
 
 ### Potential improvement plans
-TODO
+
+1. Make symbols to be configurable.
+2. Add logging service to keep the error, warning and info logs.
+3. If we have logs, we can have the alarm to alert us when the API call's error rate is too high. Or some logic errors are too high.
+4. Create env for it (dev, prod) so we can make all the configs to be env (chainUrl, chainID, contractAddress, wallet keys, Price server endpoint)
+5. Can add some integrate test or even more unit test cases.
+6. Code coverage, Linter, and CI/CD are also good to be added.
+7. Can dockerize it to make local testing portable for others.
+8. Add alarm to check if the feeder is still running even if no error logs. (can implement health check service/have another metric to check if there is no executing logs for a long time)
--- src/feeder.ts
+++ src/feeder.ts
@@ -1,8 +1,9 @@
 import { Price } from "./client/price";
 import { Oracle } from "./service/oracle";
 
-// IMPROVEMENT: add this currencies to config
-const currencies = ["BTC", "LUNA", "ETH"];
+// IMPROVEMENT: add this symbols to config
+const symbols = ["BTC", "LUNA", "ETH"];
+const updatingThreshold = 0.1;
 
 export async function executeFeeder(
   priceClient: Price,
@@ -24,31 +25,31 @@ export async function executeFeeder(
 
 async function updatePrice(oracleClient: Oracle, prices: any): Promise<void> {
   await Promise.all(
-    currencies.map(async (currency) => {
+    symbols.map(async (symbol) => {
       try {
         // get current price from contract
-        const oldPrice = await oracleClient.getCurrentPrice(currency);
+        const oldPrice = await oracleClient.getCurrentPrice(symbol);
 
         // skip if the symbol is not available from price server
-        if (!prices[currency]) return;
+        if (!prices[symbol]) return;
 
         console.log(
-          `update ${currency} from old = ${oldPrice} to new = ${prices[currency]}`
+          `update ${symbol} from old = ${oldPrice} to new = ${prices[symbol]}`
         );
 
         // if existing price > 0, need to check different percentage
         if (oldPrice > 0.0) {
-          let diff = Math.abs(prices[currency] - oldPrice);
+          let diff = Math.abs(prices[symbol] - oldPrice);
           let percent = (diff / oldPrice) * 100.0;
           console.log(`diff is ${percent}%`);
 
           // if diff <= 0.1%, no need to update
-          if (percent <= 0.1) return;
+          if (percent <= updatingThreshold) return;
         }
 
         // update price in contract
         console.log(`update is executed`);
-        await oracleClient.updatePrice(currency, prices[currency]);
+        await oracleClient.updatePrice(symbol, prices[symbol]);
       } catch (e) {
         // IMPROVEMENT: add alarm if error late is too high
         console.log((e as Error).message);
